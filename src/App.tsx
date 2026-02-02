6938e81cef54e5d9db72cd7398eafc8273157684
kvn2004 <knimsaravihanga@gmail.com>
2026-02-02 20:23:49 +0530

    feat: Initialize a new React portfolio application with core UI components, sections, layout, styling, and assets.

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Layout } from './components/layout/Layout';
+import Hero from './components/sections/Hero';
+import About from './components/sections/About';
+import Experience from './components/sections/Experience';
+import Skills from './components/sections/Skills';
+import Projects from './components/sections/Projects';
+import Contact from './components/sections/Contact';
+
+function App() {
+  return (
+    <Layout>
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
+    </Layout>
+  );
+}
+
+export default App;
